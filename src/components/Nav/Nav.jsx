a04c894a5b1a5d5f402a7e699573a2f6f433070c
Анастасія Савченко <savyle4ka13@gmail.com>
2025-08-27 11:44:32 +0100

    add reset and variables css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,24 @@
 const Nav = () => {
-  return <div>Nav</div>;
-};
-
-export default Nav;
+  return (
+    <nav aria-label="Головна навігація">
+      <ul>
+        <li>
+          <a href="#about">Про нас</a>
+        </li>
+        <li>
+          <a href="#brands">Бренди</a>
+        </li>
+        <li>
+          <a href="#catalog">Каталог</a>
+        </li>
+        <li>
+          <a href="#order">Замовити</a>
+        </li>
+        <li>
+          <a href="#contacts">Контакти</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+export default Nav
